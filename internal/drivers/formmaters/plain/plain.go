39aeeae70c26809ee99d31c2ce03fcb495a47b3c
Солтанов Александр Назимович <a.soltanov@k2t.app>
2025-10-21 20:58:19 +0300

    review: fix errors

--- internal/drivers/formmaters/plain/plain.go
+++ internal/drivers/formmaters/plain/plain.go
@@ -14,7 +14,7 @@ func (f Formatter) Format(nodes []diff.DiffNode) (string, error) {
 
 	f.writeNodes(&sb, nodes, "")
 
-	return sb.String(), nil
+	return sb.String() + "\n", nil
 }
 
 func (f Formatter) writeNodes(sb *strings.Builder, nodes []diff.DiffNode, key string) {
@@ -32,23 +32,23 @@ func (f Formatter) writeNodes(sb *strings.Builder, nodes []diff.DiffNode, key st
 		addInterval := false
 		switch n.Type {
 		case diff.NodeNested:
-			f.writeNodes(sb, n.Children, getKey(n.Key)+".")
+			f.writeNodes(sb, n.Children, n.Key+".")
 			addInterval = true
 		case diff.NodeUnchanged:
 		case diff.NodeRemoved:
-			_, err := fmt.Fprintf(sb, "Property '%s' was removed", getKey(n.Key))
+			_, err := fmt.Fprintf(sb, "Property %s' was removed", getKey(n.Key))
 			if err != nil {
 				return
 			}
 			addInterval = true
 		case diff.NodeAdded:
-			_, err := fmt.Fprintf(sb, "Property '%s' was added with value: %s", getKey(n.Key), f.stringify(n.NewValue))
+			_, err := fmt.Fprintf(sb, "Property %s' was added with value: %s", getKey(n.Key), f.stringify(n.NewValue))
 			if err != nil {
 				return
 			}
 			addInterval = true
 		case diff.NodeUpdated:
-			_, err := fmt.Fprintf(sb, "Property '%s' was updated. From %s to %s", getKey(n.Key), f.stringify(n.OldValue), f.stringify(n.NewValue))
+			_, err := fmt.Fprintf(sb, "Property %s' was updated. From %s to %s", getKey(n.Key), f.stringify(n.OldValue), f.stringify(n.NewValue))
 			if err != nil {
 				return
 			}
@@ -69,7 +69,7 @@ func (f Formatter) stringify(v any) string {
 	case map[string]any:
 		return "[complex value]"
 	case string:
-		return fmt.Sprintf("'%s'", m)
+		return fmt.Sprintf("%s'", m)
 	case nil:
 		return "null"
 	default:
