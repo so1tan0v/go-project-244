a1058be1b9ad9d85de81e6840437e123df53fad1
Солтанов Александр Назимович <a.soltanov@k2t.app>
2025-10-21 21:38:33 +0300

    1

--- internal/drivers/formmaters/plain/plain.go
+++ internal/drivers/formmaters/plain/plain.go
@@ -14,7 +14,8 @@ func (f Formatter) Format(nodes []diff.DiffNode) (string, error) {
 
 	f.writeNodes(&sb, nodes, "")
 
-	return sb.String() + "\n", nil
+	// Always return a single trailing newline for compatibility with tests
+	return strings.TrimRight(sb.String(), "\n") + "\n", nil
 }
 
 func (f Formatter) writeNodes(sb *strings.Builder, nodes []diff.DiffNode, key string) {
@@ -29,33 +30,33 @@ func (f Formatter) writeNodes(sb *strings.Builder, nodes []diff.DiffNode, key st
 	}
 
 	for i, n := range sorted {
-		addInterval := false
+		beforeLen := sb.Len()
+		keyValue := getKey(n.Key)
+
 		switch n.Type {
 		case diff.NodeNested:
-			f.writeNodes(sb, n.Children, getKey(n.Key)+".")
-			addInterval = true
+			f.writeNodes(sb, n.Children, keyValue+".")
 		case diff.NodeUnchanged:
+			continue
 		case diff.NodeRemoved:
-			_, err := fmt.Fprintf(sb, "Property '%s' was removed", getKey(n.Key))
+			_, err := fmt.Fprintf(sb, "Property '%s' was removed", keyValue)
 			if err != nil {
 				return
 			}
-			addInterval = true
 		case diff.NodeAdded:
-			_, err := fmt.Fprintf(sb, "Property '%s' was added with value: %s", getKey(n.Key), f.stringify(n.NewValue))
+			_, err := fmt.Fprintf(sb, "Property '%s' was added with value: %s", keyValue, f.stringify(n.NewValue))
 			if err != nil {
 				return
 			}
-			addInterval = true
 		case diff.NodeUpdated:
-			_, err := fmt.Fprintf(sb, "Property '%s' was updated. From %s to %s", getKey(n.Key), f.stringify(n.OldValue), f.stringify(n.NewValue))
+			_, err := fmt.Fprintf(sb, "Property '%s' was updated. From %s to %s", keyValue, f.stringify(n.OldValue), f.stringify(n.NewValue))
 			if err != nil {
 				return
 			}
-			addInterval = true
 		}
 
-		if addInterval && i < len(sorted)-1 {
+		wroteSomething := sb.Len() > beforeLen
+		if wroteSomething && i < len(sorted)-1 {
 			_, err := fmt.Fprintf(sb, "\n")
 			if err != nil {
 				return
